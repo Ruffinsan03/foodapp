90958b283532121031b4ae6edcd2e22fcae41694
Wilisa Ruffin <51328590+Ruffinsan03@users.noreply.github.com>
2019-07-23 22:34:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var menu = {fries: 3, burger: 2};
 var input = document.querySelector('input');
 var button = document.querySelector('button');
@@ -5,30 +7,19 @@ button.addEventListener('click', function(){
     var customerOrder = input.value;
     customerOrder = customerOrder.split(',');
     //console.log(customerOrder);
-
     for(var i = 0; i < customerOrder.length; i++){
-        
+ 
         var customerItem = customerOrder[i].split(':');
-        //console.log(customerItem);
-
-    }
-
-    for(var i = 0; i < menu.length; i++){
-        
-        var menuItem = menu[i].split(':');
-        console.log(menuItem);
-
-        
-        }
-//console.log(menuItem[0]);
-
         for(var item in menu) {
-           customerItem += menu[item] 
-           
-           console.log(item); }
-
-            
-//} else{
-
-
-})
+            //    customerItem = menu[item]; 
+               console.log(customerItem[0], item);
+              if(item === customerItem[0]){
+                  console.log("Thanks for your order!")
+              } else {
+                //   console.log(item);
+                //   console.log(menu[item]);
+                 console.log("Not on menu");
+              }
+            }
+        }
+    })
